97808d431442afdf9d6ed0b6975500a027501659
Kingili22 <ali.iliyan22@gmail.com>
2021-07-12 13:33:27 -0500

    fix commands no worky

--- src/main/java/me/kingili/init/Main.java
+++ src/main/java/me/kingili/init/Main.java
@@ -9,22 +9,23 @@ import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "kingtils", name = "Kingtils", version = "1.0")
+@Mod(modid = "kingtils", name = "Kingtils", version = "1.0", clientSideOnly = true)
 
 public class Main {
     @Mod.EventHandler
-    public void onFMLInitialization(FMLInitializationEvent event) {
-        // $USER = The username of the currently logged in user.
-        // Simply prints out Hello, $USER.
-        MinecraftForge.EVENT_BUS.register(new ReadMessage());
-        MinecraftForge.EVENT_BUS.register(new ShiftRender());
+    public void preFMLInitialization(FMLPreInitializationEvent event) {
         ClientCommandHandler.instance.registerCommand(new HelpCommand());
         ClientCommandHandler.instance.registerCommand(new SetShiftXPosition());
         ClientCommandHandler.instance.registerCommand(new SetShiftYPosition());
         ClientCommandHandler.instance.registerCommand(new SetNoShiftText());
         ClientCommandHandler.instance.registerCommand(new SetShiftText());
         ClientCommandHandler.instance.registerCommand(new SetShiftColor());
+    };
+    public void onFMLInitialization(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new ReadMessage());
+        MinecraftForge.EVENT_BUS.register(new ShiftRender());
         System.out.println("Hello, " + Minecraft.getMinecraft().getSession().getUsername() + "!");
     }
 }
